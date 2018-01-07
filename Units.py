c2e8da4fa5340f04829c5c8b978edf18d663b0d1
K-Ellis <K-Ellis@users.noreply.github.com>
2018-01-07 12:46:59 +0000

    create unit class program

---
+++ Units.py
@@ -0,0 +1,8 @@
+class Unit:
+
+    def __init__(self, isBlack, total_health, black_dice, red_dice, current_damage):
+        self.isBlack = isBlack
+        self.total_health = total_health
+        self.black_dice = black_dice
+        self.red_dice = red_dice
+        self.current_damage = current_damage
